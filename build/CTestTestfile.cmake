e532a6ff4efb354f5444f11a75d9b63d1b62b659
Ethanperry247 <ethanperry247@gmail.com>
2022-03-18 17:47:39 -0600

    removed build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/project2/src
-# Build directory: /home/ubuntu/project2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("reinforcement")
